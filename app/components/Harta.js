3833eb3c1c242c7a10b47fecade5b44a3e330904
P1X3l1111 <iurietihon11@gmail.com>
2026-06-03 17:51:54 +0300

    Harta: update map to Sarmizegetusa 92, Chisinau store location

--- app/components/Harta.js
+++ app/components/Harta.js
@@ -4,7 +4,7 @@ export default function Harta() {
       <div style={{ width: "100%" }}>
         <div style={{ width: "100%", height: 450, overflow: "hidden" }}>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d90929.0153543756!2d28.7775781!3d47.0245117!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40c97c3628b769a1%3A0x37d1d6305749dd3c!2sECCO%20Shoes!5e0!3m2!1sro!2smd!4v1714000000000"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2718.0!2d28.8817099!3d46.980252!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40c9794212f4da21%3A0x4f477090435505a0!2sSarmizegetusa+St+92%2C+Chi%C8%99in%C4%83u%2C+Moldova!5e0!3m2!1sro!2smd!4v1748900000000"
             width="100%"
             height="100%"
             style={{ border: 0 }}
